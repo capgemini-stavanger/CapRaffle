b21304a50d7ba49ae913c0a6b02996728407c0eb
Bente Andorsen <benteca@gmail.com>
2011-09-20 08:05:05 +0200

    Changed the CategoryController to using Category.cs instead of CategoryViewModel. It is now possible to view existing Categories from repository, and create new ones.

--- CapRaffle/Models/CategoryListViewModel.cs
+++ CapRaffle/Models/CategoryListViewModel.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using CapRaffle.Domain.Model;
 
 namespace CapRaffle.Models
 {
     public class CategoryListViewModel
     {
-        public IEnumerable<CategoryViewModel> Categories { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
 
     }
 }
