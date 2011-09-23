ba62f5f583574cfb0e930cd5745ba06135dd8cdc
Bente Andorsen <benteca@gmail.com>
2011-09-23 09:23:16 +0200

    Added DrawWinnerViewModel

---
+++ CapRaffle/Models/DrawWinnerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CapRaffle.Domain.Model;
+
+namespace CapRaffle.Models
+{
+    public class DrawWinnerViewModel
+    {
+        public int NumberOfSpotsLeft { get; set; }
+
+        public List<Winner> Winners { get; set; }
+    }
+}
