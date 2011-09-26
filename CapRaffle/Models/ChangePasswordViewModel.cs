f3cdbca58adfc319d5657b96504b778063886f24
Stig Rune Malterud <stigrune@gmail.com>
2011-09-26 09:07:56 +0200

    Removed some files by mistake last commit,added again

---
+++ CapRaffle/Models/ChangePasswordViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace CapRaffle.Models
+{
+    public class ChangePasswordViewModel
+    {
+
+        [Required(ErrorMessage = "Please enter your capgemini.com email address")]
+        [RegularExpression(".+\\@capgemini.com",
+        ErrorMessage = "Please enter a valid capgemini.com email address")]
+        [HiddenInput(DisplayValue = false)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password requierd")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm requierd")]
+        [DataType(DataType.Password)]
+        [Display(Name="Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string PasswordAgain { get; set; }
+    }
+}
