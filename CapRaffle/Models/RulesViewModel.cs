bd2f49c4c93fd19ef6fdedc002ca01de83e2d57f
Arne Aase <arneaase@gmail.com>
2011-10-10 13:18:03 +0200

    Added missing files in previous commit

---
+++ CapRaffle/Models/RulesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CapRaffle.Domain.Model;
+using CapRaffle.Domain.Rules;
+
+namespace CapRaffle.Models
+{
+    public class RulesViewModel
+    {
+
+        public List<Rule> AvailableRules { get; set; }
+        public List<RuleParameter> RulesForEvent { get; set; }
+        public int EventId { get; set; }
+    }
+}
---
+++ CapRaffle/Models/SaveRuleViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CapRaffle.Models
+{
+    public class SaveRuleViewModel
+    {
+        public int RuleId { get; set; }
+        public int Param { get; set; }
+
+    }
+
+    public class RuleComparer : IEqualityComparer<SaveRuleViewModel>
+    {
+        public bool Equals(SaveRuleViewModel x, SaveRuleViewModel y)
+        {
+            if(object.ReferenceEquals(x,y)) return true;
+            if(object.ReferenceEquals(x, null) || object.ReferenceEquals(y, null)) return false;
+            return x.RuleId == y.RuleId;
+        }
+
+        public int GetHashCode(SaveRuleViewModel rule)
+        {
+            if(object.ReferenceEquals(rule, null)) return 0;
+            return rule.RuleId == 0 ? 0 : rule.RuleId.GetHashCode();
+        }
+    }
+}
