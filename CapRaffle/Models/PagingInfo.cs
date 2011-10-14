32af630d8661e3916511a034cd0538c77a89c89c
Stig Rune Malterud <stigrune@gmail.com>
2011-10-14 08:47:33 +0200

    Added PagingInfo model

---
+++ CapRaffle/Models/PagingInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CapRaffle.Models
+{
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        public int TotalPages
+        {
+            get { return (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage); }
+        }
+    }
+}
