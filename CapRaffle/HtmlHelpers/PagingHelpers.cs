04ab00a60fb437c23bcd04312264b87de0e12c37
Stig Rune Malterud <stigrune@gmail.com>
2011-10-14 12:51:14 +0200

    Fixed pagination so it will limit its links when the event list is long

--- CapRaffle/HtmlHelpers/PagingHelpers.cs
+++ CapRaffle/HtmlHelpers/PagingHelpers.cs
@@ -13,18 +13,50 @@ namespace CapRaffle.HtmlHelpers
         public static MvcHtmlString PageLinks(this HtmlHelper html, PagingInfo pagingInfo, Func<int, string> pageUrl)
         {
             StringBuilder result = new StringBuilder();
-            for (int i = 1; i <= pagingInfo.TotalPages; i++)
+            if (pagingInfo.TotalPages < 6)
             {
-                string archive = "";
-                if (pagingInfo.Archive) archive = "&Archive=true";
-                TagBuilder tag = new TagBuilder("a");
-                tag.MergeAttribute("href", pageUrl(i) + archive);
-                tag.InnerHtml = i.ToString();
-                if (i == pagingInfo.CurrentPage)
-                    tag.AddCssClass("selected");
-                result.Append(tag.ToString());
+                for (int i = 1; i <= pagingInfo.TotalPages; i++)
+                {
+                    TagBuilder tag = GenerateLink(i, pagingInfo);
+                    result.Append(tag.ToString());
+                }
+                return MvcHtmlString.Create(result.ToString());
             }
-            return MvcHtmlString.Create(result.ToString());
+            else
+            {
+                TagBuilder tag = GenerateLink(1, pagingInfo);
+                result.Append(tag.ToString());
+
+                //Display dots if there is a gap between page 1 and n-2.
+                if (pagingInfo.CurrentPage - 4 > 0) result.Append("..");
+
+                //Dispaly links to n-2 n-1 n n+1 n+2
+                for (int i = pagingInfo.CurrentPage-2; i <= pagingInfo.CurrentPage+2; i++)
+                {
+                    if (i > 1 && i < (pagingInfo.TotalPages))
+                    {
+                        tag = GenerateLink(i, pagingInfo);
+                        result.Append(tag.ToString());
+                    }
+                }
+                //Display dots if there is a gap between last page and n+2
+                if (pagingInfo.TotalPages - pagingInfo.CurrentPage > 3) result.Append("..");
+
+                tag = GenerateLink(pagingInfo.TotalPages, pagingInfo);
+                result.Append(tag.ToString());
+                return MvcHtmlString.Create(result.ToString());
+            }
+        }
+        
+        private static TagBuilder GenerateLink(int page, PagingInfo pagingInfo) 
+        {
+            string archive = "";
+            if (pagingInfo.Archive) archive = "&Archive=true";
+            TagBuilder tag = new TagBuilder("a");
+            tag.MergeAttribute("href", "?page="+page + archive);
+            tag.InnerHtml = page+"";
+            if (page == pagingInfo.CurrentPage) tag.AddCssClass("selected");
+            return tag;
         }
     }
 }
