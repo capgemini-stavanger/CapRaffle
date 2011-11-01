64f49f1972974b4aceeec2e723cc347bc28d5b28
Arne Aase <arneaase@gmail.com>
2011-11-01 16:32:13 +0100

    Added test for setSelectedMenuAttribute

---
+++ CapRaffle.UnitTests/AttributeTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using CapRaffle.ActionFilterAttributes;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Rhino.Mocks;
+using Moq;
+
+namespace CapRaffle.UnitTests
+{
+    [TestFixture]
+    class AttributeTests : Shared
+    {
+        [Test]
+        public void Can_get_correct_viewbag_from_set_selected_menu_attribute()
+        {
+            var attribute = new SetSelectedMenuAttribute();
+            var routedata = new RouteData();
+            routedata.Values.Add("controller", "Event");
+            routedata.Values.Add("action", "Index");
+            var httpcontext = new Mock<HttpContextBase>(MockBehavior.Loose);
+            var context = new ControllerContext(httpcontext.Object, routedata, eventController);
+
+            var filtercontext = new ActionExecutingContext(
+                context, 
+                Rhino.Mocks.MockRepository.GenerateStub<ActionDescriptor>(), 
+                new Dictionary<string, object>());
+            
+            attribute.OnActionExecuting(filtercontext);
+
+            Assert.AreEqual("Event", eventController.ViewBag.MenuController);
+            Assert.AreEqual("Index", eventController.ViewBag.MenuAction);
+        }
+    }
+}
