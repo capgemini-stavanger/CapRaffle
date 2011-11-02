ca9cb5ea1aa87b3112f997db74dc8c6c42cc05fd
Stig Rune Malterud <stigrune@gmail.com>
2011-11-02 08:24:44 +0100

    Added files for new tests

---
+++ CapRaffle.UnitTests/HomeControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using CapRaffle.Controllers;
+using System.Web.Mvc;
+
+namespace CapRaffle.UnitTests
+{
+    class HomeControllerTests
+    {
+
+        private HomeController homeController;
+        
+        [SetUp]
+        public void Setup()
+        {
+            homeController = new HomeController();
+        }
+
+        [Test]
+        public void Can_About_Return_View()
+        {
+            var res = homeController.About();
+            Assert.IsInstanceOf(typeof(ActionResult), res);
+        }
+
+        [Test]
+        public void Can_Error_Return_View()
+        {
+            var res = homeController.Error();
+            Assert.IsInstanceOf(typeof(ActionResult), res);
+        }
+    }
+}
---
+++ CapRaffle.UnitTests/StatisticControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Web.Mvc;
+
+namespace CapRaffle.UnitTests
+{
+    [TestFixture]
+    public class StatisticControllerTests : Shared
+    {
+        [Test]
+        public void Can_Get_Category_Statistics_View()
+        {
+            var result = statisticController.Category(1);
+            Assert.IsInstanceOf(typeof(ActionResult), result);
+        }
+
+        [Test]
+        public void Can_Get_User_Statistic_Partial_View()
+        {
+            var result = statisticController.UserStatisticPartial();
+            Assert.IsInstanceOf(typeof(PartialViewResult), result);
+        }
+    }
+}
